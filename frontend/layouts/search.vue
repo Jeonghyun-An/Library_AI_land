bc6db600ec342d2ec8b91fc39f6df755d64982df
Hyonii <131145818+Jeonghyun-An@users.noreply.github.com>
2026-01-29 11:31:01 +0900

    [Feat] 퍼블 들어간 Search 페이지 기준 일반 템플릿 추가

--- frontend/layouts/search.vue
+++ frontend/layouts/search.vue
@@ -1,3 +1,24 @@
 <template>
   <slot />
 </template>
+
+<script setup lang="ts">
+useHead({
+  script: [
+    // jQuery 먼저
+    { src: "/js/jquery-3.6.0.js", defer: true },
+    { src: "/js/jquery-ui.js", defer: true },
+    { src: "/js/slick.min.js", defer: true },
+    { src: "/js/swiper.js", defer: true },
+    // 퍼블 공통
+    { src: "/js/common.js", defer: true },
+  ],
+  link: [
+    { rel: "stylesheet", href: "/css/reset.css" },
+    { rel: "stylesheet", href: "/css/common.css" },
+    { rel: "stylesheet", href: "/css/layout.css" },
+    { rel: "stylesheet", href: "/css/slick.css" },
+    { rel: "stylesheet", href: "/css/swiper.css" },
+  ],
+});
+</script>
